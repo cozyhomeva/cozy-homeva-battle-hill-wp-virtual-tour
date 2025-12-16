c43e80c7169d251f0d22ce4cc3f3abfa8150e639
cozyhomeva <github@cozyhomeva.com>
2025-12-16 09:56:40 -0500

    Initial commit with Marzipano tour

---
+++ data.js
@@ -0,0 +1,821 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-front-of-house",
+      "name": "Front of House",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.8065664564355313,
+        "pitch": 0.09304979027408145,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6652933207179395,
+          "pitch": 0.07992172040071743,
+          "rotation": 0,
+          "target": "1-first-floor"
+        },
+        {
+          "yaw": -2.4625559927365597,
+          "pitch": 0.3507130705855168,
+          "rotation": 0,
+          "target": "16-driveway--parking"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-first-floor",
+      "name": "First Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.10756191228677103,
+        "pitch": 0.06075881230604274,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.371727510812553,
+          "pitch": 0.6405155757217713,
+          "rotation": 0,
+          "target": "4-living-room"
+        },
+        {
+          "yaw": 2.883084622700574,
+          "pitch": 0.8411983099283873,
+          "rotation": 0,
+          "target": "0-front-of-house"
+        },
+        {
+          "yaw": -0.2795634111335552,
+          "pitch": -0.07490685318697743,
+          "rotation": 0,
+          "target": "8-second-floor"
+        },
+        {
+          "yaw": -0.017818952790824127,
+          "pitch": 0.2811829763783358,
+          "rotation": 0,
+          "target": "2-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.23953507680917774,
+        "pitch": 0.15244542416268914,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22534660505421478,
+          "pitch": 0.7922903259010976,
+          "rotation": 4.71238898038469,
+          "target": "7-shared-bathroom"
+        },
+        {
+          "yaw": 0.2660387090615135,
+          "pitch": 0.5173122386871576,
+          "rotation": 0,
+          "target": "6-comfy-room-at-cozy-homeva-battle-hill-wp"
+        },
+        {
+          "yaw": -2.633775316944117,
+          "pitch": 0.5485657288740207,
+          "rotation": 0,
+          "target": "3-hallway-ii"
+        },
+        {
+          "yaw": 2.9201436715936557,
+          "pitch": 0.5046202323945934,
+          "rotation": 4.71238898038469,
+          "target": "1-first-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hallway-ii",
+      "name": "Hallway II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -1.6540304770709895,
+        "pitch": 0.11868086523139709,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3756245024403668,
+          "pitch": 0.6066468486706782,
+          "rotation": 0,
+          "target": "2-hallway"
+        },
+        {
+          "yaw": 0.8002685448469027,
+          "pitch": 0.57089244002408,
+          "rotation": 0,
+          "target": "5-kitchen"
+        },
+        {
+          "yaw": 2.337119052411807,
+          "pitch": 0.5958308863493951,
+          "rotation": 0,
+          "target": "15-balcony"
+        },
+        {
+          "yaw": -1.0265556424076063,
+          "pitch": 0.8202817258643087,
+          "rotation": 1.5707963267948966,
+          "target": "1-first-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-living-room",
+      "name": "Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.0012404374646362015,
+        "pitch": 0.09695626431340187,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4990568845351442,
+          "pitch": 0.3995093572292987,
+          "rotation": 0,
+          "target": "5-kitchen"
+        },
+        {
+          "yaw": 2.6121299590490015,
+          "pitch": 0.5826447043597636,
+          "rotation": 0,
+          "target": "1-first-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.2980305203069271,
+        "pitch": 0.018622194290077942,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.056572937148072,
+          "pitch": 0.5561444430797788,
+          "rotation": 0,
+          "target": "3-hallway-ii"
+        },
+        {
+          "yaw": -0.9161531834341865,
+          "pitch": 0.4728326952734374,
+          "rotation": 0,
+          "target": "4-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-comfy-room-at-cozy-homeva-battle-hill-wp",
+      "name": "Comfy Room At Cozy Homeva Battle Hill WP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.05470812175255979,
+        "pitch": 0.12714586125493454,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0106633366160134,
+          "pitch": 0.628100453947642,
+          "rotation": 0,
+          "target": "2-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-shared-bathroom",
+      "name": "Shared Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.1910170908227844,
+        "pitch": 0.2650799288700565,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8633954369376324,
+          "pitch": 0.8651287901127009,
+          "rotation": 0,
+          "target": "2-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-second-floor",
+      "name": "Second Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.42321185072380096,
+        "pitch": 0.11489017013305158,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7809593525945644,
+          "pitch": 0.735080957784545,
+          "rotation": 0,
+          "target": "9-skyline-view-at-cozy-homeva-battle-hill-wp"
+        },
+        {
+          "yaw": -0.9018479632416021,
+          "pitch": 0.14030731856453826,
+          "rotation": 0,
+          "target": "11-historic-view-at-cozy-homeva-battle-hill-wp"
+        },
+        {
+          "yaw": -1.3987479281748687,
+          "pitch": 0.1501656474724946,
+          "rotation": 4.71238898038469,
+          "target": "12-penthouse-suite-at-cozy-homeva-battle-hill-wp"
+        },
+        {
+          "yaw": -0.8000952147862783,
+          "pitch": 0.9153559688230786,
+          "rotation": 0,
+          "target": "1-first-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-skyline-view-at-cozy-homeva-battle-hill-wp",
+      "name": "Skyline View At Cozy Homeva Battle Hill WP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.5551475564897022,
+        "pitch": 0.10775558317169498,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18430503651342178,
+          "pitch": 0.16291514429407172,
+          "rotation": 0,
+          "target": "10-upstairs-shared-bathroom"
+        },
+        {
+          "yaw": -2.1438635161450232,
+          "pitch": 0.7654014607674888,
+          "rotation": 0,
+          "target": "8-second-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-upstairs-shared-bathroom",
+      "name": "Upstairs Shared Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.10993710946726232,
+        "pitch": 0.2764954496908558,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.501066904862114,
+          "pitch": 0.7626192261413252,
+          "rotation": 0,
+          "target": "11-historic-view-at-cozy-homeva-battle-hill-wp"
+        },
+        {
+          "yaw": 2.004262962939295,
+          "pitch": 0.810134991592701,
+          "rotation": 0,
+          "target": "9-skyline-view-at-cozy-homeva-battle-hill-wp"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-historic-view-at-cozy-homeva-battle-hill-wp",
+      "name": "Historic View At Cozy Homeva Battle Hill WP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.9179366048811648,
+        "pitch": 0.25511041940815105,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.939264114155394,
+          "pitch": 0.43776958095629936,
+          "rotation": 0,
+          "target": "8-second-floor"
+        },
+        {
+          "yaw": 1.7187369040870415,
+          "pitch": 0.45692714262861145,
+          "rotation": 0,
+          "target": "10-upstairs-shared-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-penthouse-suite-at-cozy-homeva-battle-hill-wp",
+      "name": "Penthouse Suite At Cozy Homeva Battle Hill WP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.31676828565006687,
+        "pitch": 0.08220134990808958,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.467042237662122,
+          "pitch": 0.4613047243664603,
+          "rotation": 0,
+          "target": "14-private-bathroom"
+        },
+        {
+          "yaw": 2.790354937725678,
+          "pitch": 0.5008125825693508,
+          "rotation": 1.5707963267948966,
+          "target": "8-second-floor"
+        },
+        {
+          "yaw": -1.853742253979627,
+          "pitch": 0.4705130998473326,
+          "rotation": 0,
+          "target": "13-private-balcony"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-private-balcony",
+      "name": "Private Balcony",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.3483691363220309,
+        "pitch": 0.015987450124649527,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4450525877699594,
+          "pitch": 0.3333997633767538,
+          "rotation": 0,
+          "target": "12-penthouse-suite-at-cozy-homeva-battle-hill-wp"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-private-bathroom",
+      "name": "Private Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 3.0654880035166503,
+        "pitch": 0.30721346076036227,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4086637243325697,
+          "pitch": 0.7227115276922014,
+          "rotation": 0,
+          "target": "12-penthouse-suite-at-cozy-homeva-battle-hill-wp"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-balcony",
+      "name": "Balcony",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.02243439916337664,
+        "pitch": 0.0349101677436785,
+        "fov": 2.0904881219625167
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2431191284999024,
+          "pitch": 0.3082605120302748,
+          "rotation": 1.5707963267948966,
+          "target": "3-hallway-ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-driveway--parking",
+      "name": "Driveway + Parking",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.6114154267816545,
+        "pitch": 0.0815856572093363,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9711814693704905,
+          "pitch": 0.3178935836986838,
+          "rotation": 0,
+          "target": "17-parking"
+        },
+        {
+          "yaw": 0.9896184433912296,
+          "pitch": 0.08859753055174302,
+          "rotation": 0,
+          "target": "0-front-of-house"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-parking",
+      "name": "Parking",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.18651980051240002,
+        "pitch": -0.12151355546948395,
+        "fov": 2.090529785162516
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6849881611543616,
+          "pitch": 0.08638296018554925,
+          "rotation": 0,
+          "target": "16-driveway--parking"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
